4cec2e313c350c5d3a95da050a7746b7ac049260
CutoNaito <loliknightowo@gmail.com>
2022-09-14 17:18:37 +0200

    efkajfloise

--- script.js
+++ script.js
@@ -3,17 +3,29 @@ var mapa = new SMap(JAK.gel("mapa"), stred, 10);
 mapa.addDefaultLayer(SMap.DEF_BASE).enable();
 mapa.addDefaultControls();
 
+function generateRandomString() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 14; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
 var data = {
     "zachod1": "50°5'0.511\"N 14°27'25.039\"E",
     "zachod2": "50°4'31.412\"N 14°25'33.571\"E",
-    "Kost": "50°29'24.83\"N,15°8'6.38\"E"
+    "Kost": "50°29'24.83\"N,15°8'6.38\"E",
+    "beroun": "50°1'33.591\"N 14°15'19.529\"E",
+    "dalsi zachod": "50°3'23.133\"N 14°19'4.474\"E"
 };
 
 var markers = [];
 var souradnice = [];
-var card = new SMap.Card();
 
 for(var xname in data){
+    var card = new SMap.Card();
     var x = SMap.Coords.fromWGS84(data[xname]);
 
     var options = {
@@ -21,11 +33,11 @@ for(var xname in data){
         anchor: {left:10, bottom:1}
     }
 
-    var random_number = Math.floor(Math.random() * 11);
-    if(random_number > 5){
+    var random_number = Math.random();
+    if(random_number > 0.5){
         card.getBody().innerHTML = "Verejne WC, zdarma.";
     } else {
-        card.getBody().innerHTML = "WC je na kod";
+        card.getBody().innerHTML = "Kod: " + generateRandomString();
     }
 
     var znacka = new SMap.Marker(x, null, options);
@@ -42,4 +54,4 @@ for(var i=0; i<markers.length; i++){
 }
 
 var cz = mapa.computeCenterZoom(souradnice);
-mapa.setCenterZoom(cz[0], cz[1]);  
+mapa.setCenterZoom(cz[0], cz[1]);
