fa6aac69e57e8a91be9d2dca95283479f678ce56
CutoNaito <loliknightowo@gmail.com>
2022-10-16 18:17:47 +0200

    wooo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var stred = SMap.Coords.fromWGS84(14.41, 50.08);
-var mapa = new SMap(JAK.gel("mapa"), stred, 10);
+const stred = SMap.Coords.fromWGS84(14.41, 50.08);
+const mapa = new SMap(JAK.gel("mapa"), stred, 10);
 mapa.addDefaultLayer(SMap.DEF_BASE).enable();
 mapa.addDefaultControls();
 
@@ -32,47 +32,58 @@ function appendForm(){
     return form;
 }
 
-var data = {
-    "zachod1": "{'sourandice' : '50°5'0.511\"N 14°27'25.039\"E', 'isPaid' : 'true'}", 
-    "zachod2": "{'souradnice' : '50°4'31.412\"N 14°25'33.571\"E', 'isPaid : 'false'}",
-    "Kost": "{'souradnice' : '50°29'24.83\"N,15°8'6.38\"E', 'isPaid : 'false'}",
-    "beroun": "{'souradnice' : '50°1'33.591\"N 14°15'19.529\"E', 'isPaid : 'false'}",
-    "dalsi zachod": "{'souradnice' : '50°3'23.133\"N 14°19'4.474\"E', 'isPaid : 'false'}"
+// var data = {
+//     "zachod1": "{'sourandice' : '50°5'0.511\"N 14°27'25.039\"E', 'isPaid' : 'true'}",
+//     "zachod2": "{'souradnice' : '50°4'31.412\"N 14°25'33.571\"E', 'isPaid : 'false'}",
+//     "Kost": "{'souradnice' : '50°29'24.83\"N,15°8'6.38\"E', 'isPaid : 'false'}",
+//     "beroun": "{'souradnice' : '50°1'33.591\"N 14°15'19.529\"E', 'isPaid : 'false'}",
+//     "dalsi zachod": "{'souradnice' : '50°3'23.133\"N 14°19'4.474\"E', 'isPaid : 'false'}"
+// };
+
+
+const data = {
+    "zachod1": {
+        "sourandice" : "50°5'0.511\"N 14°27'25.039\"E",
+        "isPaid" : true
+    },
+    "zachod2": {
+        "sourandice" : "50°4'31.412\"N 14°25'33.571\"E",
+        "isPaid" : false
+    }
 };
 
-console.log(data[1].zachod1.souradnice);
+const markers = [];
+const coordinates = [];
 
-var markers = [];
-var coordinates = [];
+for(let xname in data){
+    let card = new SMap.Card();
+    let x = SMap.Coords.fromWGS84(data[xname].sourandice);
 
-for(var xname in data){
-    var card = new SMap.Card();
-    var x = SMap.Coords.fromWGS84(data[xname].souradnice);
-
-    var options = {
+    let options = {
         title:xname,
         anchor: {left:10, bottom:1}
     }
 
-    if(xname.isPaid == 'false'){
+    if(!data[xname].isPaid){
         card.getBody().innerHTML = "Verejne WC, zdarma.";
-    } else {
+    }
+    if(data[xname].isPaid){
         card.getBody().appendChild(appendForm());
         appendForm().addEventListener("submit", getValueInput(appendForm().firstChild.value, xname));
     }
 
-    var znacka = new SMap.Marker(x, null, options);
+    let znacka = new SMap.Marker(x, null, options);
     znacka.decorate(SMap.Marker.Feature.Card, card);
     markers.push(znacka);
     coordinates.push(x);
 }
 
-var vrstva = new SMap.Layer.Marker();
+let vrstva = new SMap.Layer.Marker();
 mapa.addLayer(vrstva);
 vrstva.enable();
-for(var i=0; i<markers.length; i++){
+for(let i=0; i<markers.length; i++){
     vrstva.addMarker(markers[i]);
 }
 
-var cz = mapa.computeCenterZoom(coordinates);
+let cz = mapa.computeCenterZoom(coordinates);
 mapa.setCenterZoom(cz[0], cz[1]);
